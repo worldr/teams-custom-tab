d48b9f378f5557a0bde70f2cb31334da79e6c083
Michael <michael@valarian.com>
2024-02-02 12:32:15 +0000

    Updated MS Graph integration to Fri Feb  2 12:32:15 UTC 2024

--- src/Base-dev.vue
+++ src/Base-dev.vue
@@ -19,13 +19,13 @@ export default class Base extends Vue {
 	isDebugLogVisible = false;
 
 	beforeMount(): void {
-		console.log("Before main app mounted... Fri Feb  2 11:28:58 UTC 2024");
+		console.log("Before main app mounted... Fri Feb  2 12:32:15 UTC 2024");
 		this.startAuthFlowWithTeams();
 	}
 
 	mounted(): void {
 		console.log("Main app mounted", document.location.href);
-		console.debug("VERSION Fri Feb  2 11:28:58 UTC 2024", main().version);
+		console.debug("VERSION Fri Feb  2 12:32:15 UTC 2024", main().version);
 	}
 
 	private async startAuthFlowWithTeams(): Promise<void> {
--- src/core/Version-dev.ts
+++ src/core/Version-dev.ts
@@ -44,7 +44,7 @@ export function getVersion(path?: string): Promise<Readonly<Version>> {
 		return Promise.resolve(VERSION);
 	}
 	const p = `${path ?? VERSION_PATH}?${new Date().getTime()}`;
-	console.debug("Loading version Fri Feb  2 11:28:58 UTC 2024 info from", p);
+	console.debug("Loading version Fri Feb  2 12:32:15 UTC 2024 info from", p);
 	return fetch(p)
 		.then(response => {
 			if (response.status === 200) {
