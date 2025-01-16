b356fe2a24dedbf9fb498a7f8cfafcdf91e899fe
Michael <michael@valarian.com>
2025-01-16 08:29:01 +0000

    Updated MS Graph integration to Thu Jan 16 08:28:54 UTC 2025

--- src/Base-dev.vue
+++ src/Base-dev.vue
@@ -19,13 +19,13 @@ export default class Base extends Vue {
 	isDebugLogVisible = false;
 
 	beforeMount(): void {
-		console.log("Before main app mounted... Wed Jan 15 08:29:33 UTC 2025");
+		console.log("Before main app mounted... Thu Jan 16 08:28:54 UTC 2025");
 		this.startAuthFlowWithTeams();
 	}
 
 	mounted(): void {
 		console.log("Main app mounted", document.location.href);
-		console.debug("VERSION Wed Jan 15 08:29:33 UTC 2025", main().version);
+		console.debug("VERSION Thu Jan 16 08:28:54 UTC 2025", main().version);
 	}
 
 	private async startAuthFlowWithTeams(): Promise<void> {
