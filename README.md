41db2f090a4de727b6ec4ab7f2f1298e9d94e15e
Michael <michael@valarian.com>
2025-02-21 08:30:01 +0000

    Updated MS Graph integration to Fri Feb 21 08:29:46 UTC 2025

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 * TypeScript 4.8
 * [Docker, latest](https://docs.docker.com/desktop/)
 
-Thu Feb 20 08:30:17 UTC 2025
+Fri Feb 21 08:29:46 UTC 2025
--- src/core/AuthManager-dev.ts
+++ src/core/AuthManager-dev.ts
@@ -22,7 +22,7 @@ export class AuthManager {
 
 	async startMsFlow(): Promise<void> {
 		const msState = Promise.resolve({});
-		console.info("AUTHORISATION Thu Feb 20 08:30:17 UTC 2025:", {
+		console.info("AUTHORISATION Fri Feb 21 08:29:46 UTC 2025:", {
 			"Has ms auth data": ms != null,
 		});
 		if (msState == null) {
