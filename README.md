6264a232edec80254e918d13c158262bbee2a6c9
Michael <michael@valarian.com>
2025-05-12 08:31:43 +0000

    Updated MS Graph integration to Mon May 12 08:31:36 UTC 2025

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 * TypeScript 4.8
 * [Docker, latest](https://docs.docker.com/desktop/)
 
-Sun May 11 08:30:36 UTC 2025
+Mon May 12 08:31:36 UTC 2025
--- src/core/Version-dev.ts
+++ src/core/Version-dev.ts
@@ -44,7 +44,7 @@ export function getVersion(path?: string): Promise<Readonly<Version>> {
 		return Promise.resolve(VERSION);
 	}
 	const p = `${path ?? VERSION_PATH}?${new Date().getTime()}`;
-	console.debug("Loading version Sun May 11 08:30:36 UTC 2025 info from", p);
+	console.debug("Loading version Mon May 12 08:31:36 UTC 2025 info from", p);
 	return fetch(p)
 		.then(response => {
 			if (response.status === 200) {
