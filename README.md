ec3c46b7abd271fe2536a60b1dced328f10e868d
Michael <michael@valarian.com>
2025-07-13 08:31:12 +0000

    Updated MS Graph integration to Sun Jul 13 08:31:05 UTC 2025

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 * TypeScript 4.8
 * [Docker, latest](https://docs.docker.com/desktop/)
 
-Sat Jul 12 08:31:08 UTC 2025
+Sun Jul 13 08:31:05 UTC 2025
--- src/Base-dev.vue
+++ src/Base-dev.vue
@@ -19,13 +19,13 @@ export default class Base extends Vue {
 	isDebugLogVisible = false;
 
 	beforeMount(): void {
-		console.log("Before main app mounted... Sat Jul 12 08:31:08 UTC 2025");
+		console.log("Before main app mounted... Sun Jul 13 08:31:05 UTC 2025");
 		this.startAuthFlowWithTeams();
 	}
 
 	mounted(): void {
 		console.log("Main app mounted", document.location.href);
-		console.debug("VERSION Sat Jul 12 08:31:08 UTC 2025", main().version);
+		console.debug("VERSION Sun Jul 13 08:31:05 UTC 2025", main().version);
 	}
 
 	private async startAuthFlowWithTeams(): Promise<void> {
