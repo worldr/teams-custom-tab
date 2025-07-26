b9c3044a59e1c5d2907490022b6beaa97564dcaa
Michael <michael@valarian.com>
2025-07-26 08:31:30 +0000

    Updated MS Graph integration to Sat Jul 26 08:31:21 UTC 2025

--- src/core/AuthManager-dev.ts
+++ src/core/AuthManager-dev.ts
@@ -22,7 +22,7 @@ export class AuthManager {
 
 	async startMsFlow(): Promise<void> {
 		const msState = Promise.resolve({});
-		console.info("AUTHORISATION Fri Jul 25 08:31:45 UTC 2025:", {
+		console.info("AUTHORISATION Sat Jul 26 08:31:21 UTC 2025:", {
 			"Has ms auth data": ms != null,
 		});
 		if (msState == null) {
--- src/main-dev.ts
+++ src/main-dev.ts
@@ -50,7 +50,7 @@ export const initializer = Promise.all([getVersion(), getConfig()])
 			console.error(err);
 		};
 		app.config.warnHandler = (msg, vm, trace) => {
-			console.warn("Vue warning Fri Jul 25 08:31:45 UTC 2025:", msg, new Error().stack ?? "");
+			console.warn("Vue warning Sat Jul 26 08:31:21 UTC 2025:", msg, new Error().stack ?? "");
 			console.warn(trace);
 		};
 
