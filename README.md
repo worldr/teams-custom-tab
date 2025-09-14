96978de39c337619fe7e54be4eb6a1f0f9dbbba1
Michael <michael@valarian.com>
2025-09-14 08:29:50 +0000

    Updated MS Graph integration to Sun Sep 14 08:29:42 UTC 2025

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 * TypeScript 4.8
 * [Docker, latest](https://docs.docker.com/desktop/)
 
-Sat Sep 13 08:29:37 UTC 2025
+Sun Sep 14 08:29:42 UTC 2025
--- src/main-dev.ts
+++ src/main-dev.ts
@@ -50,7 +50,7 @@ export const initializer = Promise.all([getVersion(), getConfig()])
 			console.error(err);
 		};
 		app.config.warnHandler = (msg, vm, trace) => {
-			console.warn("Vue warning Sat Sep 13 08:29:37 UTC 2025:", msg, new Error().stack ?? "");
+			console.warn("Vue warning Sun Sep 14 08:29:42 UTC 2025:", msg, new Error().stack ?? "");
 			console.warn(trace);
 		};
 
